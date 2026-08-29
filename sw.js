d7ace798e4f24cfdc851e0a7049a415f9e95e3d6
italyanchefpizza <akifyalcin93@gmail.com>
2026-08-30 00:00:02 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="icp-menu-v6";
+const CACHE="icp-menu-v7";
 const CORE=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",event=>{
